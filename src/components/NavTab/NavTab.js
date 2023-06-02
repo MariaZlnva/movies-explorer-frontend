d42b96da1a9e94493e08d815665385e79f4cd1aa
Maria Zelenova <zma611@yandex.ru>
2023-06-02 09:09:52 +0300

    add NavTab

---
+++ src/components/NavTab/NavTab.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './NavTab.css';
+
+function NavTab () {
+  return(
+    <a className='promo__link hover' href='#about-project'>Узнать больше</a>
+  )
+};
+
+export default NavTab;
--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './Promo.css';
+import NavTab from '../NavTab/NavTab';
 import promologo from '../../images/promo-logo.svg';
 
 function Promo () {
@@ -9,7 +10,7 @@ function Promo () {
       <div className='promo__container'>
         <h1 className='promo__title'>Учебный проект студентки факультета Веб-разработки.</h1>
         <p className='promo__subtitle'>Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
-        <a className='promo__link hover' href='#about-project'>Узнать больше</a>
+        <NavTab />
       </div>
       <img className='promo__img' src={promologo} alt = ''/>
 
