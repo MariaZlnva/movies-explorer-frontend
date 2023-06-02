a08a6ef8f3b75dfdf2b5a317e0d9a73aaeb4ddf5
Maria Zelenova <zma611@yandex.ru>
2023-06-02 23:06:00 +0300

    add AboutProject adaptive all size

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,21 +2,21 @@ import React from "react";
 
 import './Main.css'
 import Promo from '../Promo/Promo';
+import AboutProject from '../AboutProject/AboutProject';
+import Techs from '../Techs/Techs';
 
 function Main () {
   return (
       <main className='content'>
         <Promo />
+        <AboutProject />
+        <Techs />
         {/* 
-        <section className='about-project' id='about-project'></section>
-        <section className='techs'></section>
+        
         <section className='about-me'></section>
         <section className='portfolio'></section> */}
       </main>
     // <>
-    //   <Promo />
-    //   <NavTab />
-    //   <AboutProject />
     //   <Techs />
     //   <AboutMe />
     //   <Portfolio />
