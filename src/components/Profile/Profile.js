5fedaf3aefb02e6c065fe9843d97b331bc664ed2
Maria Zelenova <zma611@yandex.ru>
2023-06-08 11:49:43 +0300

    add Profile adaptive all size

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,7 +1,28 @@
 import './Profile.css';
+import Header from '../Header/Header';
 function Profile () {
   return(
-    <></>
+    <>
+    <Header isLoggedIn={true}/>
+    <div className='profile'>
+      <h3 className='profile__title'>Привет, --name--!</h3>
+      <form className='profile__form'>
+        <fieldset className='profile__fieldsets'>
+          <label className='profile__label'>
+            Имя
+            <input className='profile__input' type='text' name='nameProfile' value='--name--' required ></input>
+          </label>
+          <label className='profile__label'>
+            E-mail
+          <input className='profile__input' type='email' name='emailProfile' value='pochta@yandex.ru' required ></input>
+          </label>
+        </fieldset>
+        <button className='profile__btn-edit' type='submit'>Редактировать</button>
+        <button className='profile__btn-exit' type='button'>Выйти из аккаунта</button>
+      </form>
+    </div>
+    </>
+    
   )
 }
 
