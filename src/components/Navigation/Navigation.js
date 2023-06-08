17cc708ee07a61ba19322ef429e81be1c5c2514b
Maria Zelenova <zma611@yandex.ru>
2023-06-08 14:32:17 +0300

    add burger icon

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,16 +1,22 @@
-import React from "react";
-import { Link, Routes, Route } from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 import './Navigation.css'
 // import iconAvat from '../../images/foto__avatar';
 
+function Navigation({ isLoggedIn }) {
+  const [isBurgerOpen, setIsBurgerOpen] = useState(false);
 
-function Navigation({isLoggedIn}) {
+  const handleClickBurger = () => {
+    setIsBurgerOpen(!isBurgerOpen);
+  }
+
   return (
-      <div className = 'navBar'>
+      <div className = 'navBar navBar_burgerPosition'>
         {
           isLoggedIn ? 
-            <div className = 'navBar__logged'>
+          <>
+           <div className = 'navBar__logged'>
               <div className='navBar__wrap'>
                 <Link to={'/movies'} className='navBar__link-logged'>Фильмы</Link>
                 <Link to={'/saved-movies'} className='navBar__link-logged'>Сохранённые фильмы</Link>
@@ -20,11 +26,36 @@ function Navigation({isLoggedIn}) {
                 <span className='navBar__profile-logo'></span>
               </Link>
             </div>
+            <button className="navBar__burger" onClick={handleClickBurger}>
+              <span
+                className={
+                  isBurgerOpen
+                    ? "navBar__burgerLine navBar__burger_active"
+                    : "navBar__burgerLine"
+                }
+              ></span>
+              <span
+                className={
+                  isBurgerOpen
+                    ? "navBar__burgerLine navBar__burger_active"
+                    : "navBar__burgerLine"
+                }
+              ></span>
+              <span
+                className={
+                  isBurgerOpen
+                    ? "navBar__burgerLine navBar__burger_active"
+                    : "navBar__burgerLine"
+                }
+              ></span>
+            </button>
+          </>
           : <div className='navBar__unLogged'>
               <Link to={'/signup'} className='navBar__link-unLogged'>Регистрация</Link>
               <Link to={'/signin'} className='navBar__link-unLogged'>Войти</Link>
             </div>
         } 
+
       </div>
   )
 } 
